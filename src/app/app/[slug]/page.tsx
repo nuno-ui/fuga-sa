1e6a57ea2048a52c827d9bd2d4c5b8fe8f1b23c3
nuno-ui <nuno@goabeto.com>
2026-02-03 13:54:49 +0100

    v2: tudo controlado pelo Supabase

--- src/app/app/[slug]/page.tsx
+++
@@ -1,6 +0,0 @@
-import FugaApp from "@/components/FugaApp";
-
-export default async function GroupPage({ params }: { params: Promise<{ slug: string }> }) {
-  const { slug } = await params;
-  return <FugaApp slug={slug} />;
-}
